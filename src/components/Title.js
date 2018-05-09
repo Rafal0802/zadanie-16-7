050de861965905d79c930288539fbffe9fad3b99
Rafal Swieton <rafal@eskrzynka.pl>
2018-05-09 02:37:43 +0200

    Add and change files

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import style from './Title.css';
 
 const Title = props => (
-		<h1>{props.title} ({props.number})</h1>
+		<h1>{props.title} (<span>{props.number}</span>)</h1>
 );
 
 export default Title;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from './Todo.css';
 
 const Todo = props => (
 	<li key={props.todo.id} onClick={() => props.remove(props.todo.id)}>{props.todo.text}</li>
